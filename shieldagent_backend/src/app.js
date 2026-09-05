ef8c42f0e93f9b825e0d27288e85a77ce2e509e8
gajanandk9753 <info.kumawatgajanand890@gmail.com>
2026-09-05 15:26:35 +0530

    added log

--- shieldagent_backend/src/app.js
+++ shieldagent_backend/src/app.js
@@ -8,6 +8,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
+    next();
+});
+
 //auth
 app.use("/auth", authRoutes);
 
